84b89ee3ce3ec07cd8eeed1e1946d65d6806827e
Elm-R <last.one@yahoo.com>
2025-07-26 20:41:15 +0200

    added new features to update medicine quantity in inventory and show expired medicines

--- medicineChild.py
+++ medicineChild.py
@@ -11,7 +11,7 @@ class medicineChildClass(medicineParentClass):
 
         #print(self.set_inventory_csv_file_path())
         #print(self.config)
-        print("CSV File Data:", self.read_csv_file())
+        #print("CSV File Data:", self.read_csv_file())
 
         # add a medicine to inventory
         #self.add_medicine_to_inventory("Paracetamol", 50, "Tablet", "2025-12-31", "500mg", "Pain relief", "2023-10-01")
@@ -19,7 +19,10 @@ class medicineChildClass(medicineParentClass):
         # delete a medicine from inventory
         #self.delete_medicine_from_inventory("Paracetamol", 50, "Tablet", "2025-12-31", "500mg", "Pain relief", "2023-10-01")
 
-        
+        # update a medicine's quantity in inventory
+        #self.update_medicine_quantity("Digoxin","tablet", "02/10/2026", "0.25mg", 20)
+
+        print("List of expired medicines:", self.show_expired_meds())
 
 
 if __name__ == "__main__":
--- medicineParent.py
+++ medicineParent.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import csv
 import os
+from datetime import datetime
 
 
 # Class Constructor
@@ -12,6 +13,7 @@ class medicineParentClass:
         self.config = None  # to store configuration data
         self.load_config() # initialize config before using it
         self.inventory_csv_file = self.set_inventory_csv_file_path()
+        self.csv_data = self.read_csv_file()
 
     def set_json_file(self):
         return "config.json"
@@ -93,7 +95,53 @@ class medicineParentClass:
             print("No matching row found to delete.")
 
 
-        
-       
-        
+    def update_medicine_quantity(self, name, type, expiry_date, 
+                                 dosage, new_quantity_in_packages):
 
+        #csv_data = self.read_csv_file() 
+        if self.csv_data is None:
+           print("No data to update.")
+           return None
+       
+         # Find the row to update
+        condition = (
+              (self.csv_data["name"] == name) &
+              (self.csv_data["type"] == type) &
+              (self.csv_data["expiry_date"] == expiry_date) &
+              (self.csv_data["dosage"] == dosage)
+        )
+
+        print("Searching for:", name, type, expiry_date, dosage)
+        
+        if condition.any():
+            # Update the quantity in packages for the matching row
+            self.csv_data.loc[condition, "quantity_in_packages"] = new_quantity_in_packages
+            self.csv_data.to_csv(self.inventory_csv_file, index=False)
+            
+            print(f"Medicine {name} quantity updated successfully.")
+        else:
+            print("No matching row found to update.")
+
+
+    # show expired and to expire in x days meds
+
+    def show_expired_meds(self):
+
+        if self.csv_data is None:
+           print("No data to update.")
+           return None     
+        
+        todays_date = datetime.today().strftime('%Y-%m-%d')
+        
+        #convert expiry dates into datetime format for comparison
+        self.csv_data["expiry_date"] = pd.to_datetime(self.csv_data["expiry_date"], format='%Y-%m-%d')
+
+        expired_meds = self.csv_data [self.csv_data ["expiry_date"] < todays_date]
+        # print number of rows
+        print("number of rows is:", len(expired_meds))
+        # Set display options
+        pd.set_option('display.max_rows', None)
+        pd.set_option('display.max_columns', None)
+        #print("List of expired medicines:", expired_meds)
+        return expired_meds
+        
